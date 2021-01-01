f0c757e2267a05d42609200ed2efbc3afc7da3a0
spoorthi <spurti20oct@gmail.com>
2021-01-01 22:57:39 +0530

    new progs added

---
+++ Day 3/Tressure Island  game.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('would you like to take left or right?\n').lower()
+if choice1 == 'left':
+  choice2=input('you are in island,would you like to swim or wait?\n ').lower()
+  if choice2 == 'wait':
+
+    choice3 = input('you have 3 doors red, yellow or blue ?').lower()
+
+    if choice3 == 'red':
+      print('room full of fire game over')
+    elif choice3 == 'yellow':
+      print("you found the treasure you win")
+    elif choice3 == 'blue':
+      print('eaten by beastes game over')
+    else:
+      print('dont recognise the choice game over')
+  else:
+    print('you are attacked by wild trout game over')
+
+else:
+  print('you fell in to whole Game over')
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
